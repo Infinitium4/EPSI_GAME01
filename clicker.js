8cdd4ce4bbd9ea1fa44e7d144abd04f0b1af5602
Iooro <pageot.gab1@gmail.com>
2025-11-25 16:39:46 +0100

    correctifs 1

--- clicker.js
+++ clicker.js
@@ -1,6 +1,11 @@
-            let compteur = 0;
+let compteur = 0;
 
-            document.getElementById("bouton").addEventListener("click", function() {
-                compteur++;
-                document.getElementById("affichage").textContent = compteur;
-            });
+// Récupère l'affichage et le bouton
+const affichage = document.getElementById('affichage');
+const btn = document.getElementById('button');
+
+// À chaque clic, on ajoute 1 et on met à jour le span
+btn.addEventListener('click', () => {
+  compteur += 1;
+  affichage.textContent = compteur;
+});
